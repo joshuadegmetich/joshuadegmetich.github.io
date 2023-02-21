a24f7aca561a659f3933e6c70d78605c250b0c00
joshuadegmetich <89993177+joshuadegmetich@users.noreply.github.com>
2023-02-20 16:15:46 -0800

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,12 @@
+function warningMessage() {
+    alert("Hello, world!");
+    document.getElementById("textInput").style.fontWeight="bold";
+    document.getElementById("textInput").style.color="blue";
+    document.getElementById("textInput").style.textDecoration="underline";
+}
+function warningMessageBoring() {
+    alert("Hello, world!");
+    document.getElementById("textInput").style.fontWeight="normal";
+    document.getElementById("textInput").style.color="black";
+    document.getElementById("textInput").style.textDecoration="none";
+}
